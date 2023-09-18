fe8a31016e60f35beb39b7393b8e46f85bb5d7ef
Paolo Medel <paolomedel17@gmail.com>
2023-09-18 11:30:34 -0400

    day 4 right before login functionality

--- src/components/welcome/Welcome.js
+++ src/components/welcome/Welcome.js
@@ -1,4 +1,6 @@
+import { Button } from "react-bootstrap";
 import "./Welcome.css";
+import { Link } from "react-router-dom";
 
 export const Welcome = () => {
   return (
@@ -7,6 +9,20 @@ export const Welcome = () => {
         <audio autoPlay controls>
           <source src="./audio/[YT2mp3.info] - Pokémon Ruby, Sapphire & Emerald - Gym Leader Battle Music (HQ) (128kbps).mp3" />
         </audio>
+      </div>
+      <p>Welcome to PokeStop!</p>
+      <div>
+        <p>Paragraph explaining what this site is for</p>
+      </div>
+      <div>
+        <Link to={`/pokebox/`}>
+          <Button>Click here to see your Pokemon</Button>
+        </Link>
+      </div>
+      <div>
+        <Link to={`/pokedex/`}>
+          <Button>Click here to go to the PokeDex</Button>
+        </Link>
       </div>
     </main>
   );
